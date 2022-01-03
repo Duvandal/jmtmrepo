f9aae6408979766b0e3918da735cbe375b55dc77
duvandal <tio.angelbert@gmail.com>
2022-01-03 15:32:24 +0700

    'update'

--- admin/index.php
+++ admin/index.php
@@ -8,17 +8,19 @@ if ($_SESSION['Username']) {
 
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>JMTM Repository</title>
-        <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
         <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
         <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
         <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+
+
     </head>
     <body>
         <div class="navbar navbar-fixed-top">
@@ -69,41 +71,64 @@ if ($_SESSION['Username']) {
                         </div>
                         <!--/.sidebar-->
                     </div>
-                    <!--/.span3-->
-                    
+
                     <div class="span9">
-                        <center>
-                            <div class="card" style="width: 50%;"> 
-                                <div class="card-body">
+                        <?php
+                        if(isset($_POST['submit']))
+                            {$s=$_POST['Judul'];
+                                $sql="select * from repo.buku where Indeks='$s' or Perihal like '%$s%' or Tahun like '%$s%' 
+                                or Lokasi like '%$s%' or KodePelaksana like '%$s%' or Unit like '%$s%'";
+                            }
+                        else
+                            $sql="select * from repo.buku";
 
-                                <?php
-                                $username = $_SESSION['Username'];
-                                $sql="select * from repo.user where Username='$username'";
-                                $result=$conn->query($sql);
-                                $row=$result->fetch_assoc();
+                        $result=$conn->query($sql);
+                        $rowcount=mysqli_num_rows($result);
 
-                                $name=$row['Name'];
-                                $category=$row['Category'];
-                                $divisi=$row['Divisi']
-                                ?>    
-                                    <i>
-                                    <h1 class="card-title"><center><?php echo $name ?></center></h1>
-                                    <br>
-                                    <p><b>Nama: </b><?php echo $name ?></p>
-                                    <br>
-                                    <p><b>Divisi: </b><?php echo $divisi ?></p>
-                                    <br>
-                                    <p><b>Kategori: </b><?php echo $category ?></p>
-                                    </b>
-                                </i>
-
-                                </div>
-                            </div>
-                        <br>
-                        <a href="edit_admin_details.php" class="btn btn-primary">Edit Details</a>
-                        </center>               
+                        if(!($rowcount))
+                            echo "<br><center><h2><b><i>No Results</i></b></h2></center>";
+                        else
+                        {
+                        ?>
+                        <table id="dtBasicExample" class="table table-striped table-bordered" style="width:100%">
+                            <thead>
+                            <tr>
+                                <th>Kode Pelaksana</th>
+                                <th>Indeks</th>
+                                <th>Unit Kerja</th>
+                                <th>Tahun</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            
+                            //$result=$conn->query($sql);
+                            while($row=$result->fetch_assoc())
+                            {
+                                $kode=$row['KodePelaksana'];
+                                $indeks=$row['Indeks'];
+                                $unit=$row['Unit'];
+                                $thn=$row['Tahun'];
+                            
+                            ?>
+                                <tr>
+                                    <td><?php echo $kode ?></td>
+                                    <td><?php echo $indeks ?></td>
+                                    <td><?php echo $unit ?></td>
+                                    <td><?php echo $thn ?></td>
+                                </tr>
+                                <?php }} ?>
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>Kode Pelaksana</th>
+                                <th>Indeks</th>
+                                <th>Unit Kerja</th>
+                                <th>Tahun</th>
+                            </tr>
+                            </tfoot>
+                        </table>
                     </div>
-                    
                     <!--/.span9-->
                 </div>
             </div>
@@ -116,11 +141,10 @@ if ($_SESSION['Username']) {
         </div>
         
         <!--/.wrapper-->
-        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>
         <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
         <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
         <script src="scripts/common.js" type="text/javascript"></script>
       
