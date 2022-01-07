89dbe0d643bf3966f09904755aaa8297d277bbe5
duvandal <tio.angelbert@gmail.com>
2022-01-07 10:55:54 +0700

    'update'

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ require('dbconn.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Style -->
-	<link rel="stylesheet" href="style.css">
+	<!-- <link rel="stylesheet" href="style.css"> -->
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
